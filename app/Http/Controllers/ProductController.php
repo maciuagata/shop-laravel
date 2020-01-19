2493f2f415a613f6a67e2b48348e83aca83e1e8d
Agata <maciulewiczagata@gmail.com>
2020-01-19 12:58:18 +0200

    Add order inputs and put data to database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -105,12 +105,19 @@ class ProductController extends Controller
     
             Stripe::setApiKey('');
             try {
-                Charge::create(array(
+                $charge = Charge::create(array(
                     "amount" => $cart->totalPrice * 100,
                     "currency" => "eur",
                     "source" => $request->input('stripeToken'), // obtained with Stripe.js
                     "description" => "Test Charge"
                 ));
+                $order = new Order();
+                $order->cart = serialize($cart);
+                $order->address = $request->input('address');
+                $order->name = $request->input('name');
+                $order->payment_id = $charge->id;
+
+                Auth::user()->orders()->save($order);
             } catch (\Exception $e) {
                 return redirect()->route('checkout')->with('error', $e->getMessage());
             }
